4f7a1a9bce83ca96a497bb9f4b65343a07078544
Fajar Satria <fajarsat@hotmail.com>
2020-07-03 06:20:39 +0700

    cleansing code

--- app/src/main/java/com/example/java_volley/ShowData.java
+++ app/src/main/java/com/example/java_volley/ShowData.java
@@ -5,20 +5,16 @@ import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 
